6fe2c6db256412e9cdaad452fd09aaadc1867294
Sâmyla <samylamaria14@gmail.com>
2022-10-14 08:32:10 -0300

    Corrigindo

--- js/app.js
+++ js/app.js
@@ -4,29 +4,22 @@ const lstFeito = document.querySelector("#feito")
 
 const adicionarTarefa = function(ev) {
     ev.preventDefault()
-    
     const tarefa = document.querySelector("#tarefa").value
-
     if (tarefa.length < 1) {
         return;
     }
-
     const novaTarefa = document.createElement("li")
     novaTarefa.innerText = tarefa
-
     lstAFazer.appendChild(novaTarefa)
 }
 
 const moverParaFeito = function(ev) {
     const tarefa = ev.target.innerText
-
     const novaTarefa = document.createElement("li")
     novaTarefa.innerText = tarefa
-
     lstFeito.appendChild(novaTarefa)
     lstAFazer.removeChild(ev.target)
 }
 
 btnAddTarefa.addEventListener("click", adicionarTarefa)
-
 lstAFazer.addEventListener("click", moverParaFeito)
